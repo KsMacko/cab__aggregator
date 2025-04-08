698abfb3739cefc31d78f7e66e2500d051c71c91
KsMacko <Adeline1397@gmail.com>
2025-04-08 06:24:31 +0300

    Initial commit

---
+++ src/main/java/com/internship/cab__aggregator/CabAggregatorApplication.java
@@ -0,0 +1,13 @@
+package com.internship.cab__aggregator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CabAggregatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CabAggregatorApplication.class, args);
+	}
+
+}
