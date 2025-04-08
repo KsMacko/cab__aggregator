6c28e3b43bbd0262ea4341aceecd0622f0e37a7b
KsMacko <Adeline1397@gmail.com>
2025-04-08 06:36:15 +0300

    init all modules

---
+++ finance_service/src/main/kotlin/com/internship/finance_service/FinanceServiceApplication.kt
@@ -0,0 +1,11 @@
+package com.internship.finance_service
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FinanceServiceApplication
+
+fun main(args: Array<String>) {
+    runApplication<FinanceServiceApplication>(*args)
+}
