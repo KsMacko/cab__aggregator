45072bd6fa1f16344227a94c3cecd8f146ba78f0
KsMacko <Adeline1397@gmail.com>
2025-04-13 00:31:45 +0300

    added validation messages

--- driver_service/src/main/java/com/internship/driver_service/dto/ProfileDto.java
+++ driver_service/src/main/java/com/internship/driver_service/dto/ProfileDto.java
@@ -2,6 +2,7 @@ package com.internship.driver_service.dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.PositiveOrZero;
 import jakarta.validation.constraints.Size;
@@ -24,19 +25,24 @@ public record ProfileDto(
 
         @Pattern(regexp = CYRILLIC_REGEX, message = "firstName.invalidInput")
         @Size(max = MAX_NAME_LENGTH, message = "firstName.size")
+        @NotBlank(message = "firstName.notBlank")
         String firstName,
 
         @Pattern(regexp = CYRILLIC_REGEX, message = "lastName.invalidInput")
         @Size(max = MAX_NAME_LENGTH, message = "lastName.size")
+        @NotBlank(message = "lastName.notBlank")
         String lastName,
 
         @Pattern(regexp = FARE_TYPE_PATTERN, message = "fareType.invalidInput")
+        @NotBlank(message = "fareType.notBlank")
         String fareType,
 
         @Pattern(regexp = DRIVER_STATUS_PATTERN, message = "driverStatus.invalidInput")
+        @NotBlank(message = "driverStatus.notBlank")
         String driverStatus,
 
         @Pattern(regexp = PHONE_PATTERN, message = "phone.invalidInput")
+        @NotBlank(message = "phone.notBlank")
         String phone,
 
         @Schema(hidden = true)
--- driver_service/src/main/java/com/internship/driver_service/dto/RateDto.java
+++ driver_service/src/main/java/com/internship/driver_service/dto/RateDto.java
@@ -2,6 +2,7 @@ package com.internship.driver_service.dto;
 
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.PositiveOrZero;
 
 import static com.internship.driver_service.utils.validation.ValidationConstants.MAX_ID_VALUE;
@@ -11,12 +12,15 @@ import static com.internship.driver_service.utils.validation.ValidationConstants
 public record RateDto(
         @Min(value = MIN_RATE, message = "rate.minValue")
         @Max(value = MAX_RATE, message = "rate.maxValue")
+        @NotNull(message = "value.notNull")
         Byte value,
         @PositiveOrZero(message = "id.positive")
-        @Max(value = MAX_ID_VALUE, message = "profile.id.maxValue")
+        @Max(value = MAX_ID_VALUE, message = "authorId.id.maxValue")
+        @NotNull(message = "authorId.notNull")
         Long authorId,
         @PositiveOrZero(message = "id.positive")
         @Max(value = MAX_ID_VALUE, message = "profile.id.maxValue")
+        @NotNull(message = "profile.notNull")
         Long driverId
 ) {
 }
