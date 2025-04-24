0fa1cc44181a300d2d5f2aa2b0907f69f9e5f1c1
KsMacko <Adeline1397@gmail.com>
2025-04-24 10:36:43 +0300

    updated artemis data

--- driver-service/src/main/java/com/internship/driverservice/config/ArtemisConfig.java
+++ driver-service/src/main/java/com/internship/driverservice/config/ArtemisConfig.java
@@ -3,6 +3,7 @@ package com.internship.driverservice.config;
 import jakarta.jms.ConnectionFactory;
 import jakarta.jms.JMSException;
 import org.apache.activemq.artemis.jms.client.ActiveMQConnectionFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jms.core.JmsTemplate;
@@ -11,13 +12,19 @@ import org.springframework.jms.support.converter.MessageType;
 
 @Configuration
 public class ArtemisConfig {
+    @Value(value = "${spring.artemis.broker-url}")
+    private String brokerUrl;
+    @Value(value = "${spring.artemis.user}")
+    private String user;
+    @Value(value = "${spring.artemis.password}")
+    private String password;
 
     @Bean
     public ConnectionFactory connectionFactory() throws JMSException {
         ActiveMQConnectionFactory factory = new ActiveMQConnectionFactory();
-        factory.setBrokerURL("tcp://localhost:61616");
-        factory.setUser("admin");
-        factory.setPassword("admin");
+        factory.setBrokerURL(brokerUrl);
+        factory.setUser(user);
+        factory.setPassword(password);
         return factory;
     }
 
