aac9fff16428f435f8765208be5cf21c2d82944a
KsMacko <Adeline1397@gmail.com>
2025-04-24 10:52:15 +0300

    updated artemis data

--- finance-service/src/main/kotlin/com/internship/financeservice/config/ArtemisConfig.kt
+++ finance-service/src/main/kotlin/com/internship/financeservice/config/ArtemisConfig.kt
@@ -1,8 +1,8 @@
 package com.internship.financeservice.config
 
 import jakarta.jms.ConnectionFactory
-import jakarta.jms.JMSException
 import org.apache.activemq.artemis.jms.client.ActiveMQConnectionFactory
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.jms.core.JmsTemplate
@@ -11,14 +11,22 @@ import org.springframework.jms.support.converter.MessageType
 
 @Configuration
 class ArtemisConfig {
+    @Value("\${spring.artemis.broker-url}")
+    private lateinit var brokerUrl: String
+
+    @Value("\${spring.artemis.user}")
+    private lateinit var artemisUser: String
+
+    @Value("\${spring.artemis.password}")
+    private lateinit var artemisPassword: String
+
     @Bean
-    @Throws(JMSException::class)
     fun connectionFactory(): ConnectionFactory {
-        val factory = ActiveMQConnectionFactory()
-        factory.setBrokerURL("tcp://localhost:61616")
-        factory.setUser("admin")
-        factory.setPassword("admin")
-        return factory
+        return ActiveMQConnectionFactory().apply {
+            setBrokerURL(brokerUrl)
+            user = artemisUser
+            password = artemisPassword
+        }
     }
 
     @Bean
